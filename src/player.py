d0e179ab6d91bdd3a741cab7382800cfc78c0d61
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-12-03 11:43:19 +0300

    complete player movement

--- src/player.py
+++ src/player.py
@@ -7,8 +7,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft=pos)
+
+        # player movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -20,6 +24,17 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         self.get_input()
-        self.rect.x += self.direction.x * self.speed
+
+
--- src/settings.py
+++ src/settings.py
@@ -1,18 +1,17 @@
 level_map = [
-    '                ',
-    '          P X   ',
-    'X X X     XXX X ',
-    'X X X    XXXXXX ',
-    'XXXXXXXXXXXXXXX ',
-    'XXXXXX  XXXXXXX ',
-    'XXXXXXXXXXXXXXX ',
-    'XX  XXXXXXXXXXX ',
-    'XXXXXXXXXXXXXXX ',
-    'XXXXXXXXXXXXXXX ',
-    'XXXXXXXXXXXXXXX '
+    '      XXX               ',
+    '          P X           ',
+    'X X X     XXX X         ',
+    'X X X    XXXXXX         ',
+    'XXXXXXXXXXXXXXX         ',
+    'XXXXXX  XXXXXXX         ',
+    'XXXXXXXXXXXXXXXXXXXXXXXX',
+    'XX  XXXXXXXXXXX         ',
+    'XXXXXXXXXXXXXXX         ',
+    'XXXXXXXXXXXXXXX         ',
+    'XXXXXXXXXXXXXXX         '
 ]
 
 tile_size = 64
 screen_width = 1200
 screen_height = len(level_map) * tile_size
-print(screen_height)
