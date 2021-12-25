2d761f9c2c8d290f31fde96a635817c97224c8b1
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-12-25 03:32:22 +0300

    socket server

--- src/main.py
+++ src/main.py
@@ -5,40 +5,43 @@ import sys
 import pygame
 
 from settings import *
-from level import Level
-from game_data import level_0
-from overworld import Overworld
-from ui import UI
-
-
-class Game:
-    def __init__(self):
-        self.max_level = 1
-        self.overworld = Overworld(0, self.max_level, screen, self.create_level)
-        self.status = 'overworld'
-
-        self.stars = 0
-        self.ui = UI(screen)
-
-    def create_level(self, current_level):
-        self.level = Level(current_level, screen, self.create_overworld, self.change_stars)
-        self.status = 'level'
-
-    def create_overworld(self, current_level, new_max_level):
-        if new_max_level > self.max_level:
-            self.max_level = new_max_level
-        self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
-        self.status = 'overworld'
-
-    def change_stars(self, amount):
-        self.stars += amount
-
-    def run(self, keys_dict):
-        if self.status == 'overworld':
-            self.overworld.run()
-        else:
-            self.level.run(keys_dict)
-            self.ui.show_coins(self.stars)
+# from level import Level
+# from game_data import level_0
+# from overworld import Overworld
+# from ui import UI
+#
+#
+# class Game:
+#     def __init__(self):
+#         self.max_level = 1
+#         self.overworld = Overworld(0, self.max_level, screen, self.create_level)
+#         self.status = 'overworld'
+#
+#         self.stars = 0
+#         self.ui = UI(screen)
+#
+#     def create_level(self, current_level):
+#         self.level = Level(current_level, screen, self.create_overworld, self.change_stars, self.check_game_over)
+#         self.status = 'level'
+#
+#     def create_overworld(self, current_level, new_max_level):
+#         if new_max_level > self.max_level:
+#             self.max_level = new_max_level
+#         self.overworld = Overworld(current_level, self.max_level, screen, self.create_level)
+#         self.status = 'overworld'
+#
+#     def change_stars(self, amount):
+#         self.stars += amount
+#
+#     def check_game_over(self, amount):
+#         self.stars -= amount
+#
+#     def run(self, keys_dict):
+#         if self.status == 'overworld':
+#             self.overworld.run()
+#         else:
+#             self.level.run(keys_dict)
+#             self.ui.show_coins(self.stars)
 
 # connecting to the server
 host = 'localhost'
@@ -69,7 +72,7 @@ pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 clock = pygame.time.Clock()
-game = Game()
+# game = Game()
 
 while True:
     # read commands
@@ -78,16 +81,16 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # keys_dict = (keys_to_dict(pygame.key.get_pressed()))
-    keys_dict = pygame.key.get_pressed()
-    keys_to_send = json.dumps(keys_dict).encode('ascii')
+    # send a command to the server
+    try:
+        keys_dict = pygame.key.get_pressed()
+        keys_to_send = json.dumps(keys_dict).encode('ascii')
 
-    client_socket.send(keys_to_send)
+        client_socket.send(keys_to_send)
+    except:
+        pass
 
     """
-    # send a command to the server
-    client_socket.send('To left'.encode())
-
     # we get a new state of the field from the server
     data = client_socket.recv(2**20)
     data = data.decode()
@@ -96,7 +99,7 @@ while True:
     print(data)
     """
 
-    game.run(keys_dict)
+    # game.run(keys_dict)
 
     pygame.display.update()
     clock.tick(fps)
